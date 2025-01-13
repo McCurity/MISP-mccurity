22e434c2faddcb9c14f6c466b82e099867da0eae
McCurity <mccurity@outlook.com>
2025-01-13 11:35:29 -0500

    Update Event.php
    This commit updated the validFormats to include panedl for use with the new PANEDLExport class.  That class is intended primarily for attribute only exports so the addition will likely not be used for is included for consistency.

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -119,6 +119,7 @@ class Event extends AppModel
         'netfilter' => array('txt', 'NetfilterExport', 'sh'),
         'opendata' => array('txt', 'OpendataExport', 'txt'),
         'openioc' => array('xml', 'OpeniocExport', 'ioc'),
+        'panedl' => array('txt', 'PANEDLExport', txt'),
         'rpz' => array('txt', 'RPZExport', 'rpz'),
         'snort' => array('txt', 'NidsSnortExport', 'rules'),
         'stix' => array('xml', 'Stix1Export', 'xml'),
